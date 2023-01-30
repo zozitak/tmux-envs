83cc5179e6e34ca691c59314446f0bee322636b7
Takács Zoltán Gábor <zozitak@gmail.com>
2023-01-30 10:22:56 +0100

    feat: frontend base sttings changes 2

--- tmux-frontend.sh
+++ tmux-frontend.sh
@@ -14,22 +14,25 @@ tmux new-window -t $session:$window -n 'code'
 tmux send-keys -t $session:$window 'nvim .' Enter
 
 window=2
-tmux new-window -t $session:$window -n 'tests'
+tmux new-window -t $session:$window -n 'test'
 #unit tests, acceptanece tests
+tmux send-keys -t $session:$window 'mkdir test && cd test || cd test' Enter
 
 window=3
 tmux new-window -t $session:$window -n 'git'
 #SCM tool
 tmux send-keys -t $session:$window 'git status' Enter
+tmux send-keys -t $session:$window 'g tree' Enter
 
 window=4
 tmux new-window -t $session:$window -n 'docs'
 #Documentation, Open API endpoints, API testing
+tmux send-keys -t $session:$window 'mkdir docs && cd docs || cd docs' Enter
 
 window=5
 tmux new-window -t $session:$window -n 'server'
 #ssh, integration, deployment
-#tmux send-keys -t $session:$window 'npm run serve'
+#tmux send-keys -t $session:$window 'live-server -p=5000' Enter
 
 window=1
 tmux select-window -t $session:$window
